5f3838a1348406ebf69c68466553180c4175491c
Griffin Puc <griffinpuc@Outlook.com>
2020-12-04 22:15:48 -0500

    added db scripts

---
+++ Calendar/DB Scripts/create.sql
@@ -0,0 +1,39 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema calendar
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema calendar
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `calendar` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `calendar` ;
+
+-- -----------------------------------------------------
+-- Table `calendar`.`events`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `calendar`.`events` (
+  `event_id` INT NOT NULL AUTO_INCREMENT,
+  `event_name` VARCHAR(45) NOT NULL,
+  `event_desc` MEDIUMTEXT NOT NULL,
+  `event_start` DATETIME NOT NULL,
+  `event_end` DATETIME NOT NULL,
+  `event_user` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`event_id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 18
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
